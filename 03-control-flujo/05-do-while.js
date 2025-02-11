d9a8286a47b8f0a2b560e42ccdcfae6d10f5e37e
scrooge-93 <114698333+scrooge-93@users.noreply.github.com>
2025-02-11 20:48:34 -0300

    Add files via upload

---
+++ 03-control-flujo/05-do-while.js
@@ -0,0 +1,15 @@
+let i = 2;
+while (i < 2) {
+    if (i % 2 == 0) {
+        console.log('Numero par: ', i);
+    }
+    i++;
+}
+
+// En 'do while' siempre se ejecuta la porcion de codgo primero antes de la evaluación
+// do {
+//     if (i % 2 == 0) {
+//         console.log('Numero par: ', i);
+//     }
+//     i++;
+// } while (i < 2);
